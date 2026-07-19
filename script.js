e5044fd61a96672b00a2a1668c4b75c5ad3be0f7
TaquiAhmad <rounakjahaan6228@gmail.com>
2026-07-19 10:29:22 +0530

    Refactor smooth scroll and add welcome message
    Added welcome message and restructured smooth scroll functionality.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-// Smooth Scroll
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener("click", function (e) {
+// One8 Taqui Website
+
+console.log("Welcome to One8 Taqui!");
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener("click", function(e) {
     e.preventDefault();
 
     const target = document.querySelector(this.getAttribute("href"));
